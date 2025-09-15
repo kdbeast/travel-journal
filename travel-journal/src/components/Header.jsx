ec99d6f93cd265712e3235d8538aa58cacde0cc8
Karan <karanjamwal2004@gmail.com>
2025-09-15 22:36:57 +0530

    travel journal completed

---
+++ travel-journal/src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header>
+      <img src="/globe.png" alt="globe icon" />
+      <h1>My Travel Journal</h1>
+    </header>
+  );
+};
+
+export default Header;
